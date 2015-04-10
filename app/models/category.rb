4126fc4e2b4f092885af6d98d8a21c3d88d8c536
Sergey Lukuanov <slukuanov@gmailc.om>
2015-04-10 16:03:25 +0300

    changes for admin part

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,6 @@
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :books
 
-  default_scope  { order('created_at DESC') }
   scope :sort_by_id, -> { order('id') }
   scope :search, lambda{|query| where('title ILIKE :query ', {query: "%#{query}%"}) }
 
