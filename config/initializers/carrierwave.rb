627fe6d76b5807eecb6b4405839b02b987f39917
Sergey Lukuanov <slukuanov@gmailc.om>
2015-04-30 00:10:33 +0300

    setting carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,8 +8,6 @@ CarrierWave.configure do |config|
   config.fog_public     = true                                   # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
 
-  if(Rails.env.heroku?)
-    config.fog_directory  = 'funobby-dev'                     # required
-  end
+  config.fog_directory  = 'funobby-dev'                     # required
 
 end
